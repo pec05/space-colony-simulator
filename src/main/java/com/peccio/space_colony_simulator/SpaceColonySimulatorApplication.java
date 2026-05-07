5096f8b2ad4c8dedba639d76e297515bfb33dec8
pec05 <leandro-peccio@hotmail.com>
2026-05-07 12:50:49 +0200

    chore(init): initial Spring Boot project scaffold

---
+++ src/main/java/com/peccio/space_colony_simulator/SpaceColonySimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.peccio.space_colony_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceColonySimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceColonySimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peccio/space_colony_simulator/SpaceColonySimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peccio.space_colony_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceColonySimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
