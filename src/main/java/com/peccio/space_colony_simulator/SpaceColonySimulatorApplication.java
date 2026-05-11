fc089928d81cf77405ddb77188d99d7c6ecb12bb
pec05 <leandro-peccio@hotmail.com>
2026-05-11 21:52:47 +0200

    Revert "feat(simulation): add tick processor and colony simulation service"
    This reverts commit 6141a331715a3df48adac4edcc7ff28cdea70980.

--- src/main/java/com/peccio/space_colony_simulator/SpaceColonySimulatorApplication.java
+++ src/main/java/com/peccio/space_colony_simulator/SpaceColonySimulatorApplication.java
@@ -2,12 +2,10 @@ package com.peccio.space_colony_simulator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@ConfigurationPropertiesScan
 public class SpaceColonySimulatorApplication {
 
 	public static void main(String[] args) {
